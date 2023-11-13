c43b7fb581e595a5d41cf2243ea515d9c1ba8601
obenchero <othmanebenchekroun@outlook.fr>
2023-11-13 13:11:32 +0100

    added rate limit for each controller

--- production-service/src/main.ts
+++ production-service/src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
 
   const openApiOptions = new DocumentBuilder()
     .setTitle('Production API Service - Namkin X SUPINFO')
-    .setDescription('<a href="/api/v1/production/production-swagger-spec.json" target="_blank">http://localhost:3000/api/v1/production/production-swagger-spec.json</a>')
+    .setDescription('<a href="/api/v1/production/production-swagger-spec.json" target="_blank">https://localhost:3000/api/v1/production/production-swagger-spec.json</a>')
     .setVersion('v1')
     .addServer('/api/v1')
     .build();
