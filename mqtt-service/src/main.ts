b9d4f57d1889301578f955b07404c6f2ea2fa145
obenchekro <othmanebenchekroun@outlook.fr>
2024-01-12 19:46:13 +0100

    MQTT broker service condeployed

--- mqtt-service/src/main.ts
+++ mqtt-service/src/main.ts
@@ -5,15 +5,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 async function bootstrap() {
   const app = await NestFactory.create(MqttModule);
   const openApiOptions = new DocumentBuilder()
-  .setTitle('MQTT API Service - Namkin X SUPINFO')
-  .setVersion('v1')
-  .addServer('/api/v1')
-  .build();
+    .setTitle('MQTT API Service - Namkin X SUPINFO')
+    .setVersion('v1')
+    .addServer('/api/v1')
+    .build();
 
-const mqttDocument = SwaggerModule.createDocument(app, openApiOptions, {
-  include: [MqttModule]
-});
-SwaggerModule.setup('api/v1/mqtt', app, mqttDocument);
+  const mqttDocument = SwaggerModule.createDocument(app, openApiOptions, {
+    include: [MqttModule]
+  });
+  SwaggerModule.setup('api/v1/mqtt', app, mqttDocument);
 
   app.setGlobalPrefix('api/v1');
   await app.listen(3006);
